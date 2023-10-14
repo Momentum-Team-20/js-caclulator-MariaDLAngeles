9ce59826165fc7e2ff7208b58187dc42720f662a
Maria Angeles <maria.d.juri@gmail.com>
2023-10-14 17:14:36 -0400

    calculator works and cleaned up code

--- main.js
+++ main.js
@@ -1,32 +1,21 @@
-console.log('connected')
+// console.log('connected')
 
 let calculation = []
 let fullCalculation
 
 const buttons = document.querySelectorAll('button');
-// we are selecting ALL the button elements
 
 function clickButton(button) {
-    // we are actively 'listening'/ watching for an event of 'click'
-        console.log(event)
-    // we are displaying that 'click' in the console when it happens
+        // console.log(event)
         console.log(button.innerText)
-    // we are console logging the innerText from the html element that was clicked and logging it in the console
+        let displayElement = document.querySelector('#display')
+        displayElement.value = button.innerText
     
-         let displayElement = document.querySelector('#display')
-    // this returns the first element with the id of display
-            displayElement.value = button.innerText
-    //our display variable is now the button HTML's inner text 
-    
-
-    calculate(button)
+        calculate(button)
 
     function calculate(button){
-    // we created a function
+        //textContent -- a property used to set or get text content
         const value = button.textContent
-    //assinged a variable of value and used textContent -- a property used to set or get text content
-
-    
     
     if(value === "C") {
         calculation = []
@@ -35,11 +24,9 @@ function clickButton(button) {
         displayElement.value = eval(fullCalculation)
     } else {
         calculation.push(value)
-        //pushed it into our empty array above -- 'let calculation'
         fullCalculation = calculation.join('');
         // we are using .join to have all the numbers displayed as one big unit instead of separated by ,'s -- .join -> returns a new string by concatenating all of the elements in this array
         displayElement.value = fullCalculation
-        // and now will display it, to show what's happening on the screen
         }
     }
     
