2529aa0e68e2a1f2c0e3dabb1c9c08e4e5c9b147
Maria Angeles <maria.d.juri@gmail.com>
2023-10-25 16:33:07 -0400

    changed setTimout to event listener that responds to click, commented out console logs, tidied up html

--- main.js
+++ main.js
@@ -5,6 +5,50 @@ let fullCalculation
 
 const buttons = document.querySelectorAll('button');
 
+for(let button of buttons) {
+    button.addEventListener('click', ()=> clickButton(button))
+
+}
+
+// function test(input) {}
+// const test = (input)=>{a line of code}
+// const test = (input)=>
+
+
+function clickButton(button) {
+    // console.log(event)
+    // console.log(button.innerText)
+    let displayElement = document.querySelector('#display')
+    displayElement.value = button.innerText
+    
+    calculate(button)
+    
+    function calculate(button){
+
+        const value = button.innerText
+        
+        if(value === "C") {
+            calculation = []
+            displayElement.value = '' 
+        } else if (value === "=") {
+            displayElement.value = eval(fullCalculation)
+        } else {
+            calculation.push(value)
+            fullCalculation = calculation.join('');
+            // we are using .join to have all the numbers displayed as one big unit instead of separated by commas -- .join -> returns a new string by concatenating all of the elements in this array
+            displayElement.value = fullCalculation
+        }
+        
+        // this block is for the fancy card flip
+        if(value === "!") {
+            deleteCalculator()
+            console.log(button.innerText)
+            
+        } 
+        
+    }    
+}    
+
 
 // this function clears the entire contents of our calculator and replaces it with our ghost image
 function deleteCalculator() {
@@ -14,53 +58,9 @@ function deleteCalculator() {
     ghostImage.classList.add("flip-calc-ghost")
     entireCalculator.innerHTML = []
     entireCalculator.appendChild(ghostImage)
-    ghostImage.addEventListener('click',()=>
-        console.log('Ghost click!'))
-        setTimeout(() => location.reload(), 3000)
+    ghostImage.addEventListener('click',()=> {
+        // console.log('Ghost click!')
+        location.reload()
+        })
 // location.reload() is a function used to refresh your page in the browser
-//  setTimeout() delays our page from reloading for about 3 seconds
     }
-
-
-function clickButton(button) {
-        // console.log(event)
-        // console.log(button.innerText)
-        let displayElement = document.querySelector('#display')
-        displayElement.value = button.innerText
-    
-        calculate(button)
-
-    function calculate(button){
-        //textContent -- a property used to set or get text content
-        const value = button.textContent
-    
-    if(value === "C") {
-        calculation = []
-        displayElement.value = '' 
-    } else if (value === "=") {
-        displayElement.value = eval(fullCalculation)
-    } else {
-        calculation.push(value)
-        fullCalculation = calculation.join('');
-        // we are using .join to have all the numbers displayed as one big unit instead of separated by ,'s -- .join -> returns a new string by concatenating all of the elements in this array
-        displayElement.value = fullCalculation
-        }
-    
-        // this block is for the fancy ghost flip, if we can add a click before the spin that'll be even cooler
-    if(value === "!") {
-        deleteCalculator()
-        // displayElement.value = "Oh no! A ghost!"
-        console.log(button.innerText)
-        // console.log(displayElement.value)
-
-    } 
-
-    }
-}
-
-
-for(let button of buttons) {
-    button.addEventListener('click', ()=> clickButton(button))
-
-}
-    
